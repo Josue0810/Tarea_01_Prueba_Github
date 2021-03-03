3b2fe0f7d80fb1c84f4d1a164d919abd40db650b
Josue0810 <79821111+Josue0810@users.noreply.github.com>
2021-03-02 21:30:53 -0600

    Agregando programas

---
+++ Hola mundo Tu nombre.cpp
@@ -0,0 +1,11 @@
+//Actividad 1
+//Josué Barrios Lazcano
+//1 de Marzo de 2021
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	cout<<"Hola mi nombre es Josue Barrios Lazcano";
+}
